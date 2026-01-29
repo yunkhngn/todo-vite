8f19b911dbfc03bef6421e758d5da8a217b5dba7
yunkhngn <rogkhoa.mail@gmail.com>
2026-01-29 18:06:04 +0700

    feat: init project

---
+++ backend/src/main/java/com/yunkhngn/backend/TodoViteApplication.java
@@ -0,0 +1,13 @@
+package com.yunkhngn.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoViteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoViteApplication.class, args);
+	}
+
+}
