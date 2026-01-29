53a576cf23710e34ddcda493eab6408b2c47e174
yunkhngn <rogkhoa.mail@gmail.com>
2026-01-29 18:26:56 +0700

    feat: init todo and user table

---
+++ backend/src/main/java/com/yunkhngn/backend/repository/TodoRepository.java
@@ -0,0 +1,11 @@
+package com.yunkhngn.backend.repository;
+
+import com.yunkhngn.backend.entity.Todo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface TodoRepository extends JpaRepository<Todo, UUID> {
+}
---
+++ backend/src/main/java/com/yunkhngn/backend/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.yunkhngn.backend.repository;
+
+import com.yunkhngn.backend.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, UUID> {
+}
